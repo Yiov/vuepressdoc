5ddc9ca9fd64aa14a4b737790b5b5b5f3fd74423
DzPian <490857007@qq.com>
2023-07-14 10:17:21 +0800

    侧边栏配置

--- docs/.vuepress/configs/sidebar.ts
+++ docs/.vuepress/configs/sidebar.ts
@@ -1,21 +1,40 @@
-import type { SidebarConfig } from '@vuepress/theme-default'
+import type { SidebarConfig } from 'vuepress'
 
-export const sidebarA: SidebarConfig = {
-    '/guide/': [
-        {
-          text: '指南',
-          children: [
-            '/guide/README.md', 
-            '/guide/getting-started.md', 
-            '/guide/configuration.md', 
-            '/guide/page.md',
-            '/guide/markdown.md',
-            '/guide/assets.md',
-            '/guide/beautification.md', 
-            '/guide/plugin.md', 
-            '/guide/vitepress.md',
-          ],
-          collapsible: true, //折叠开关
-        },
-      ],
-}
+export const sidebarA: SidebarConfig = [
+  {
+    text: '介绍',
+    link: '/guide/',
+    children: [
+      '/guide/README.md', 
+    ],
+  },
+  {
+    text: '基础配置',
+    link: '/guide/',
+    children: [
+      '/guide/getting-started.md', 
+      '/guide/configuration.md', 
+      '/guide/page.md',
+      '/guide/frontmatter.md',
+    ],
+  },
+  {
+    text: '进阶玩法',
+    link: '/guide/',
+    children: [
+      '/guide/markdown.md',
+      '/guide/assets.md',
+      '/guide/beautification.md', 
+      '/guide/plugin.md', 
+    ],
+  },
+  {
+    text: '其他站点',
+    children: [
+      { text: 'VitePress文档搭建', link: 'https://yiov.github.io/vitepress/' },
+      { text: '劝学录教程', link: 'https://yiov.github.io/' },
+      { text: '个人主页', link: 'https://yingyayi.com/' },
+    ],
+  },
+
+]
